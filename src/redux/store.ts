2125a5e8548f07accb0d75c1e25822ded58dea32
Nar0118~ <narek@Nareks-MacBook-Pro.local>
2025-03-20 22:16:16 +0400

    removed unneseccary file

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postsReducer from './postsSlice';
 import authReducer from './authSlice';
-import { postsApi } from './postsApi';
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
     auth: authReducer,
-    [postsApi.reducerPath]: postsApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(postsApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
